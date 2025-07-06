42d81c9480aa52d2c35aa4f512da918b2bf18585
Benjamin De Kosnik <bdekosnik@mozilla.com>
2025-07-05 20:52:25 -0700

    generate-and-publish-to-github.sh: add date and platform abstraction

--- scripts/generate-and-publish-to-github.sh
+++ scripts/generate-and-publish-to-github.sh
@@ -1,10 +1,19 @@
+#!/usr/bin/env bash
 
-../../scripts/generate-video-side-by-side-wrapper.sh 2025-07-02 android-15-ptablet .
-../../scripts/generate-video-filmstrip-wrapper.sh 2025-07-02 android-15-ptablet .
-../../scripts/generate-aggregate-json.sh 2025-07-02 android-15-ptablet
+TDATE=$1
+
+TPMETADATA1="android-15-p8"
+TPMETADATA1a="android-15-p8-talkback"
+TPMETADATA2="android-15-ptablet"
+TPMETADATA2a="android-15-ptablet-talkback"
+TPMETA=$2
+
+../../scripts/generate-video-side-by-side-wrapper.sh $TDATE $TPMETA .
+../../scripts/generate-video-filmstrip-wrapper.sh $TDATE $TPMETA .
+../../scripts/generate-aggregate-json.sh $TDATE $TPMETA
 ../../scripts/generate-progressive-pageload.sh
 ../../scripts/copy-result-resources-into-places-for-display.sh
-cp index-1-col-android-15-ptablet.md ../../2025-07-02-ptablet.md
+cp index-1-col-android-15-ptablet.md ../../${TDATE}/${TPMETA}.md
 ../../scripts/check-in-results.sh
 
 
