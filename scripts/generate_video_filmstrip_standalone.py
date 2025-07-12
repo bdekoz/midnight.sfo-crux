c810d594173e27b2ce938315176eb14befd97e64
Benjamin De Kosnik <bdekosnik@mozilla.com>
2025-07-11 21:39:31 -0700

    scale filmstrips by 4

--- scripts/generate_video_filmstrip_standalone.py
+++ scripts/generate_video_filmstrip_standalone.py
@@ -128,7 +128,7 @@ def generate_video_filmstrip_control_points(ivideo, cpfilename):
                 else:
                     timecodemin = int(timecoden/60)
                     timecodesec = timecoden - timecodemin;
-                    thumbflag = "-ss 00:" + str(timecodemin) + ":" + str(timecodesec) + cspace + "-frames:v 1"
+                    thumbflag = "-ss 00:" + str(timecodemin) + ":" + str(timecodesec) + cspace + "-frames:v 1 -vf scale=iw/4:ih/4 "
                 #timecodestr = f"{timecoden:.2f}"
                 #timecodestr = f"{timecodems:05}"
                 timecodestr = f"{timecodems:05d}"
